18dac75e62f8db4c38e2d5e40785b1d3cbabb25d
Christian Rodriguez <christian@totallifechanges.com>
2019-09-02 21:19:56 -0400

    Add project files.

---
+++ Deliverable 2 - Fun Places to Go/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deliverable_2___Fun_Places_to_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
